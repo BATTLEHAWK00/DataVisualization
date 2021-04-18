5bacd8d9d975ca8534c9b65002447c919432c59d
BATTLEHAWK <battlehawk0_0@163.com>
2021-04-18 16:10:31 +0800

    提交后端Servlet编写示例

---
+++ backend/src/main/java/controller/servlet/TestServlet.java
@@ -0,0 +1,18 @@
+package controller.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/helloworld")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().println("<h1>Hello World!</h1><p>使用/hello Servlet接口输出了Hello World</p>");
+    }
+}
